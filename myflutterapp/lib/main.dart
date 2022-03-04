7bbfe64bed2da0c7e87747615f8652641695e16d
Jeremy Callinan <jeremy.callinan@gmail.com>
2022-03-03 20:18:03 -0500

    Saved suggestions and fonts

--- myflutterapp/lib/main.dart
+++ myflutterapp/lib/main.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -13,8 +13,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme:  const AppBarTheme(
+          backgroundColor: Colors.white54,
+          foregroundColor: Colors.black,
+        ),
+      ),
       home: RandomWords(),
     );
   }
@@ -30,31 +37,75 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 14.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Startup Name Generator'),
+        title: Text('Startup Name Generator',
+          style: GoogleFonts.pacifico(),),
+        actions: [IconButton(onPressed: _pushSaved, icon: const Icon(Icons.list), tooltip: 'Saved Suggestions',)],
       ),
       body: ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: /*1*/ (context, i) {
+
           if (i.isOdd) return const Divider(); /*2*/
 
           final index = i ~/ 2; /*3*/
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
+          final alreadySaved = _saved.contains(_suggestions[index]);
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
-              style: _biggerFont,
+              style: _biggerFont
             ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.pink : null,
+              semanticLabel: alreadySaved ? 'Remove from saved':'Save'
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            }
           );
         },
-      ),
+      )
+
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return ListTile(
+                title: Text(pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty ?
+              ListTile.divideTiles(tiles: tiles, context:context).toList() : <Widget>[];
+          return Scaffold(
+            appBar: AppBar(title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      )
     );
   }
 }
